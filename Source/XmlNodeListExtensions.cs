24e8a9e5d8165e4c910fb7de54d63988ead68fc2
15adhami <15adhami@gmail.com>
2025-08-16 20:47:05 -0700

    Added XmlNodeList extension methods back

---
+++ Source/XmlNodeListExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+
+namespace XmlDocumentViewer
+{
+    internal static class XmlNodeListExtensions
+    {
+        public static List<XmlNode> ToList(this XmlNodeList nodes)
+        {
+            List<XmlNode> list = [];
+            foreach (XmlNode node in nodes)
+            {
+                if (node != null) list.Add(node);
+            }
+            return list;
+        }
+    }
+}
