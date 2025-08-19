27989c462496f907a094e82812e92b8defe223bd
15adhami <15adhami@gmail.com>
2025-08-19 05:42:15 -0700

    Updated to latest C# version and added RecExtensions

---
+++ Source/RectExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace XmlDocumentViewer
+{
+    internal static class RectExtensions
+    {
+        extension(Rect rect)
+        {
+            internal Rect TrimLeftPart(float pct)
+        {
+            return rect.RightPart(1f - pct);
+        }
+
+        internal Rect TrimRightPart(float pct)
+        {
+            return rect.LeftPart(1f - pct);
+        }
+
+        internal Rect TrimTopPart(float pct)
+        {
+            return rect.BottomPart(1f - pct);
+        }
+
+        internal Rect TrimBottomPart(float pct)
+        {
+            return rect.TopPart(1f - pct);
+        }
+
+        internal Rect TrimLeftPartPixels(float pixels)
+        {
+            return rect.RightPartPixels(rect.width - pixels);
+        }
+
+        internal Rect TrimRightPartPixels(float pixels)
+        {
+            return rect.LeftPartPixels(rect.width - pixels);
+        }
+
+        internal Rect TrimTopPartPixels(float pixels)
+        {
+            return rect.BottomPartPixels(rect.height - pixels);
+        }
+
+        internal Rect TrimBottomPartPixels(float pixels)
+        {
+            return rect.TopPartPixels(rect.height - pixels);
+        }
+    }
+}
+}
